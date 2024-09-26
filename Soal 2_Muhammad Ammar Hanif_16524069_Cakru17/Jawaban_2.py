a791b2583956e31b0136998694ed622e235f81af
Hanif <amrhanif15@gmail.com>
2024-09-26 12:18:24 +0700

    initial commit

---
+++ Soal 2_Muhammad Ammar Hanif_16524069_Cakru17/Jawaban_2.py
@@ -0,0 +1,50 @@
+# markdown live preview
+# https://dillinger.io/
+
+class Robot:
+    def __init__(self, health, attackPowerRange):
+        self.health = health
+        self.attackPowerRange = attackPowerRange
+
+    def showSpecification():
+        # code
+
+        print("Specs: ...")
+
+    def upgradeShield():
+        # code
+
+        print("upgrading shield...")
+
+    def increaseStrenth():
+        # code
+
+        print("increasing strength...")
+
+class Battle: 
+    def __init__(self, robot1, robot2):
+        self.robot1 = robot1 
+        self.robot2 = robot2
+
+    def start_fight():
+        # code
+
+        print("battle between robot () and robot ()")
+
+class Game:
+    def __init__(self, items=[]):
+        self.l = items # the Game class didn't require any argument because it's a general object to start the game, or adding a new robot
+
+    def add_robot():
+        # code
+
+        print("adding new robot...")
+
+    def start_game():
+        # code
+
+        print("starting the game...")
+        # input
+        # user will be displayed a few option in the game (add robot, check robot, robot battle, etc)
+
+    
---
+++ Soal 2_Muhammad Ammar Hanif_16524069_Cakru17/readme.md
@@ -0,0 +1,11 @@
+# Battle of Robots by Muhammad Ammar Hanif
+---
+Battle of Robots created using python programming language while adopting the Object Oriented Programming (OOP) paradigm as its core functionality. The mission of the game is to be able to create a battle between created robots (robot as the object). 
+
+The OOP concept that's used in this game is Object, Class, (...). 
+
+#### Playing the Game
+(make sure that ur device already have an installed the python interpreter)
+```sh
+python3 Jawaban_2.py
+```
